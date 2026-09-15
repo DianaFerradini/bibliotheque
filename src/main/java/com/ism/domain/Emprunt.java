942cced0d08feede602d45a92915f087134f495e
Diana Ferradini <ferradinid@gmail.com>
2026-09-15 12:30:23 +0000

    fix: corriger erreur d'orthographe dans commentaire

--- src/main/java/com/ism/domain/Emprunt.java
+++ src/main/java/com/ism/domain/Emprunt.java
@@ -3,7 +3,7 @@ package com.ism.domain;
 import java.util.Date;
 
 public final class Emprunt extends AbstractEntity{
-// Emprunt est final car aucune classe ne peu hériter de Emprunt
+// Emprunt est final car aucune classe ne peut hériter de Emprunt
 
     private String nomEmprunteur;
     private Date dateEmprunt;
